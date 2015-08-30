b43bd314393212dcb97b327cd623df036c5a5929
Tyler Renelle <tylerrenelle@gmail.com>
2015-08-30 13:59:26 -0400

    start on homepage sample cards

--- client/src/app/components/front.jsx
+++ client/src/app/components/front.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import mui from 'material-ui';
 import request from 'superagent';
 
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
 import FullWidthSection from './material-ui-docs/full-width-section.jsx';
 let { Mixins, RaisedButton, Styles } = require('material-ui');
 let { StylePropable, StyleResizable } = Mixins;
@@ -25,6 +26,10 @@ export default React.createClass({
         margin: '16px auto 0 auto',
         textAlign: 'center',
         maxWidth: '575px'
+      },
+      examples: {
+        margin: '-110px auto 0 auto',
+        maxWidth: '1204px'
       },
       label: {
         color: ThemeManager.palette.primary1Color,
@@ -67,12 +72,18 @@ export default React.createClass({
       styles.h2 = this.mergeStyles(styles.h2, styles.h2WhenLarge);
     }
 
+
+    var rightIconMenu = (
+      <mui.IconMenu iconButtonElement={<mui.IconButton><MoreVertIcon /></mui.IconButton>}>
+        <mui.MenuItem>Apply</mui.MenuItem>
+        <mui.MenuItem>Like</mui.MenuItem>
+        <mui.MenuItem>Dislike</mui.MenuItem>
+      </mui.IconMenu>
+    );
+
     return (
       <div>
-        <mui.Dialog
-          actions={[{text:'Close'}]}
-          actionFocus="submit"
-          ref="dialog" >
+        <mui.Dialog ref="dialog" >
           <mui.Tabs>
             <mui.Tab label="Login" >
               <form action='/login' method='POST'>
@@ -96,18 +107,95 @@ export default React.createClass({
           iconElementRight={
             <mui.RaisedButton label='Login / Register' onTouchTap={()=>this.refs.dialog.show()} />
           }
-          iconElementLeft={false}
+          iconElementLeft={<span />}
           />
         <FullWidthSection style={styles.root}>
           <div style={styles.tagline}>
             <h1 style={styles.h1}>JobSeed</h1>
-            <h2 style={styles.h2}>
-              Pandora-like job board, find jobs tailored to you
-            </h2>
+            <h2 style={styles.h2}>Find jobs tailored to you.</h2>
           </div>
-          <div style={{paddingLeft:50, paddingRight:50}}>
-            {/*Content*/}
-          </div>
+        </FullWidthSection>
+        <FullWidthSection style={styles.examples}>
+          <mui.Card>
+            <mui.CardTitle title='Rate job posts' />
+            <mui.ListDivider />
+            <mui.CardHeader
+              title='Seeking Senior JavaScript Developer'
+              subtitle='Company, Inc | San Francisco, CA'
+              avatar="http://lorempixel.com/100/100/technics/10"
+              />
+            <mui.CardText>
+              <p>
+                Come work for the coolest company on earth! We use <b>React</b>, React Native, <b>Node.js</b>/Express, <b>Postgres</b>. PTO, paid vacation, 401k, and an HTC Vive room for competitive produce-juggling.
+              </p>
+              <mui.FloatingActionButton secondary={true}>
+                <mui.FontIcon className="material-icons">thumb_up</mui.FontIcon>
+              </mui.FloatingActionButton>
+              &nbsp;&nbsp;
+              <mui.FloatingActionButton secondary={true}>
+                <mui.FontIcon className="material-icons">thumb_down</mui.FontIcon>
+              </mui.FloatingActionButton>
+            </mui.CardText>
+          </mui.Card>
+
+          <br/>
+          <mui.Card>
+            <mui.CardTitle title='That adjusts scores' />
+            <mui.ListDivider />
+            <mui.CardText>
+              <mui.List>
+                {['React', 'Node', 'Postgres', 'San Francisco, CA', 'Company, inc.'].map(k => {
+                  return <mui.ListItem primaryText={
+                    <div><span style={{fontWeight:'bold', color:'green'}}>+1</span> {k}</div>
+                  } />
+                })}
+              </mui.List>
+            </mui.CardText>
+            <mui.CardTitle
+              title='Filtering your job-hunt'
+              subtitle='Scores train JobSeed to narrow down jobs tailored for your interests.'
+              />
+            <mui.ListDivider />
+            <mui.CardText>
+              <mui.List>
+                {
+                  [
+                    {title:'Node.js, Postgres Backend Engineer', sub:'Medical Inc. | San Jose, CA'},
+                    {title:'Python Machine Learning Expert', sub:'Singularity Ltd. | Boston, MA', disabled:true},
+                    {title:'React + Flux Web/Mobile Developer', sub:'Agency LLC | Portland, OR'},
+                    {title:'Backbone, Rails Full-Stack Dev', sub:'NotMyCup Co. | Austin, TX', disabled:true},
+                  ].map((i,n)=> <mui.ListItem
+                    style={ i.disabled ? {textDecoration:'line-through', opacity:0.5} : {}}
+                    leftAvatar={<mui.Avatar src={"http://lorempixel.com/100/100/technics/"+(n+1)} />}
+                    primaryText={i.title}
+                    secondaryText={i.sub}
+                    rightIconButton={rightIconMenu}
+                    />
+                  )
+                }
+              </mui.List>
+            </mui.CardText>
+          </mui.Card>
+
+          <br/>
+          <mui.Card>
+            <mui.CardTitle
+              title={<span>And making <i>you</i> easier to find</span>}
+              subtitle={`No more "didn't do their homework" recuiter emails, companies find you because you're a good fit.`} />
+            <mui.ListDivider />
+            <mui.CardHeader
+              title='Me'
+              subtitle='Full-stack JavaScript Developer'
+              avatar="http://lorempixel.com/100/100/people"
+              />
+            <mui.CardText>
+              <p>
+                "Me" matches your company by a score of <span style={{color:'green'}}>+10</span> on the following attributes: <b>React</b>, <b>Node</b>, <b>Postgres</b>.
+              </p>
+              <mui.RaisedButton label="Contact" />
+            </mui.CardText>
+          </mui.Card>
+
         </FullWidthSection>
       </div>
     );
