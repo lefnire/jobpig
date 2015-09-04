027907bfba2ef37881a358ef16a72b665ddf871c
Tyler Renelle <tylerrenelle@gmail.com>
2015-09-04 18:23:56 -0400

    messing with meta & buttons styling

--- client/src/app/components/jobs/job.jsx
+++ client/src/app/components/jobs/job.jsx
@@ -37,13 +37,9 @@ class Job extends React.Component {
       <mui.Card>
         <mui.CardTitle
           title={<a href={job.url} target='_blank'>{job.title}</a>}
-          subtitle={this._subtitle(job)}
+          subtitle={this._meta(job)}
         />
         <mui.CardText>
-          Tags:&nbsp;
-          <span style={{color:'rgb(0, 188, 212)', textTransform:'uppercase', fontWeight:500}}>
-            {_.pluck(job.tags, 'key').join(', ')}
-          </span>
           {editing ?
             <mui.Paper zDepth={2} style={{padding:5}}>
               <mui.TextField
@@ -59,17 +55,17 @@ class Job extends React.Component {
               <p>{job.note}</p>
             </mui.Paper>
           }
+          <mui.CardActions>{
+            ( job.status=='inbox' ? [
+              <mui.FlatButton label="Skip" onTouchTap={()=>this._setStatus('hidden')}/>,
+              <mui.FlatButton label="Mark Applied" onTouchTap={()=>this._setStatus('applied')}/>
+            ] : [
+              <mui.FlatButton label="Send to Inbox" onTouchTap={()=>this._setStatus('inbox')}/>
+            ]).concat(
+              <mui.FlatButton label="Add Note" onTouchTap={()=>JobActions.setEditing(job.id)}/>
+            )
+          }</mui.CardActions>
         </mui.CardText>
-        <mui.CardActions>{
-          ( job.status=='inbox' ? [
-            <mui.FlatButton label="Skip" onTouchTap={()=>this._setStatus('hidden')}/>,
-            <mui.FlatButton label="Mark Applied" onTouchTap={()=>this._setStatus('applied')}/>
-          ] : [
-            <mui.FlatButton label="Send to Inbox" onTouchTap={()=>this._setStatus('inbox')}/>
-          ]).concat(
-            <mui.FlatButton label="Add Note" onTouchTap={()=>JobActions.setEditing(job.id)}/>
-          )
-        }</mui.CardActions>
       </mui.Card>
 
       <mui.Card style={{background:'#f8f8f8'}}>
@@ -80,26 +76,33 @@ class Job extends React.Component {
         </mui.CardText>
       </mui.Card>
 
-      {job.status == 'inbox' ?
-        <div style={{position:'fixed',bottom:10,right:10}}>
+      {job.status=='inbox' ?
+        <mui.CardActions style={{position:'fixed', right:20, bottom: 20}}>{[
           <mui.FloatingActionButton onTouchTap={()=>this._setStatus('liked')}>
             <mui.FontIcon className="material-icons">thumb_up</mui.FontIcon>
-          </mui.FloatingActionButton>
-          &nbsp;&nbsp;
+          </mui.FloatingActionButton>,
           <mui.FloatingActionButton onTouchTap={()=>this._setStatus('disliked')}>
             <mui.FontIcon className="material-icons">thumb_down</mui.FontIcon>
-          </mui.FloatingActionButton>
-        </div>
-        : false}
+          </mui.FloatingActionButton>,
+        ]}</mui.CardActions> : false }
     </div>;
   }
 
-  _subtitle(job){
+  _meta(job){
     let score = job.score>0 ? `+${job.score}` : job.score<0 ? job.score : false;
-    return _.reduce({Source:job.source, Company:job.company, Location:job.location, Budget:job.budget, Score:score}, (m,v,k)=>{
-      if (v) m.push(`${k}: ${v}`);
-      return m;
-    }, []).join('; ');
+    let meta = _.filter([
+      {name:"Source", text:job.source, icon:'find_in_page'},
+      {name:"Company", text:job.company, icon:'supervisor_account'},
+      {name:"Location", text:job.location, icon:'room'},
+      {name:"Budget", text:job.budget, icon:'attach_money'},
+      {name:"Score", text:score, icon:'thumb_up'},
+      {name:"Tags", icon:'label', style:{color:'rgb(0, 188, 212)', textTransform:'uppercase', fontWeight:500}, text: _.pluck(job.tags, 'key').join(', ') }
+    ], 'text');
+    return <span>
+      {meta.map(m=> <mui.ListItem key={m.name} primaryText={m.text} style={m.style} leftIcon={
+        <mui.IconButton iconClassName="material-icons" tooltip={m.name}>{m.icon}</mui.IconButton>
+      } /> )}
+    </span>;
   }
 
   _setStatus(status){
