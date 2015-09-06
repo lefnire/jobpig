bd9d4e76baf59bcee3cec1c94571564158e86f5f
Tyler Renelle <tylerrenelle@gmail.com>
2015-09-06 10:07:19 -0400

    GA tracking

--- client/src/app/lib/util.js
+++ client/src/app/lib/util.js
@@ -20,15 +20,17 @@ var auth = {
     localStorage.clear();
     window.location = '/';
   }
-}
+};
 
 // Handle initial "still logged in?" check on page load
-let expire = window.localStorage.getItem('expire');
-if (expire && expire < new Date) auth.logout(); // expired, log out
-window.jwt = window.localStorage.getItem('jwt');
+(function checkAuth(){
+  let expire = window.localStorage.getItem('expire');
+  if (expire && expire < new Date) auth.logout(); // expired, log out
+  window.jwt = window.localStorage.getItem('jwt');
 
-// If logged in, intercept every request with token header
-if (jwt) request.set('x-access-token', jwt).on('request', req=>req.url = API_URL+req.url);
+  // If logged in, intercept every request with token header
+  if (jwt) request.set('x-access-token', jwt).on('request', req=>req.url = API_URL+req.url);
+});
 
 var setupHotkeys = function(shortcuts) {
   return _.reduce(shortcuts, (m,v,k)=>{
@@ -37,6 +39,17 @@ var setupHotkeys = function(shortcuts) {
     m[mode].handlers[k] = v.fn;
     return m;
   }, {default:{keys:{},handlers:{}}, editing:{keys:{},handlers:{}}});
-}
+};
+
+// Setup google analytics, defer
+window.setTimeout(function setupGoogleAnalytics(){
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create','<nconf:ga_tracking_id>', 'auto');
+  ga('send', 'pageview');
+});
 
 export default {alt, request, auth, setupHotkeys};
