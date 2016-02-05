f099a09e36fe59a8e4a453368ead869e4ba470ae
Tyler Renelle <tylerrenelle@gmail.com>
2016-02-04 19:29:10 -0500

    more employer fixes

--- client/src/app/components/employer/prospect.jsx
+++ client/src/app/components/employer/prospect.jsx
@@ -2,37 +2,76 @@ import React from 'react';
 import mui from 'material-ui';
 import _ from 'lodash';
 
-export default class Prospect extends React.Component {
-  render() {
-    let p = this.props.prospect;
-    let standardActions = [
-      { text: 'Cancel' },
-      { text: 'Send', onTouchTap: this._onDialogSubmit, ref: 'submit' }
-    ];
-    return <div>
-      <mui.Card initiallyExpanded={true}>
-        <mui.CardHeader
-          title={p.fullname}
-          subtitle={`Score: ${p.score}, Tags: ${_.pluck(p.tags,'key').join(', ')}`}
-          avatar={p.pic}
-          showExpandableButton={true} />
-        <mui.CardText expandable={true}>
-          <a href={p.linkedin_url}>LinkedIn</a>
-          <div>{p.bio}</div>
-        </mui.CardText>
-        <mui.CardActions expandable={true}>
-          <mui.RaisedButton label="Contact" onTouchTap={this._handleTouchTap} />
-        </mui.CardActions>
-      </mui.Card>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
 
-      <mui.Dialog title="Contact" actions={this.standardActions} ref="contact">
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const actions = [
+      <mui.FlatButton
+        label="Cancel"
+        secondary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <mui.FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
+    return (
+      <mui.Dialog
+        title="Contact"
+        actions={actions}
+        modal={false}
+        open={this.state.open}
+        onRequestClose={this.handleClose}
+      >
         <mui.ClearFix>
           <mui.TextField hintText="Subject" fullWidth={true}/>
           <mui.TextField hintText="Message" fullWidth={true} multiLine={true} />
         </mui.ClearFix>
       </mui.Dialog>
+    );
+  }
+}
+
+export default class Prospect extends React.Component {
+  render() {
+    let p = this.props.prospect;
+    return <div>
+      <mui.Card initiallyExpanded={true}>
+        <mui.CardHeader
+          title={p.fullname || 'Anonymous'}
+          subtitle={`Score: ${p.score}, Tags: ${_.pluck(p.tags,'key').join(', ')}`}
+          avatar={p.pic}
+          showExpandableButton={true} />
+        {p.linkedin_url ? <mui.CardText expandable={true}>
+          <a href={p.linkedin_url}>LinkedIn</a>
+          <div>{p.bio}</div>
+        </mui.CardText> : null}
+        <mui.CardActions expandable={true}>
+          <mui.RaisedButton label="Contact" onTouchTap={this._handleTouchTap} />
+        </mui.CardActions>
+      </mui.Card>
+
+      <Contact ref="contact" />
     </div>
   }
 
-  _handleTouchTap = () => this.refs.contact.show();
+  _handleTouchTap = () => this.refs.contact.handleOpen();
 }
