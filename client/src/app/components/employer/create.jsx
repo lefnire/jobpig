a7f099fb564212747bae8edfda906058179480b5
Tyler Renelle <tylerrenelle@gmail.com>
2016-02-09 09:06:10 -0500

    start using formsy-mui

--- client/src/app/components/employer/create.jsx
+++ client/src/app/components/employer/create.jsx
@@ -2,45 +2,43 @@ import React from 'react';
 import mui from 'material-ui';
 import _ from 'lodash';
 import {_fetch} from '../../helpers';
+import Formsy from 'formsy-react'
+import fui from 'formsy-material-ui';
 
 export default class CreateJob extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {open: false};
+    this.state = {
+      open: false,
+      canSubmit: false
+    };
   }
 
   render() {
-    const actions = [
-      <mui.FlatButton
-          label="Cancel"
-          secondary={true}
-          onTouchTap={this.handleClose}
-      />,
-      <mui.FlatButton
-          label="Submit"
-          primary={true}
-          onTouchTap={this._createJob}
-      />,
-    ];
-
     //Handle on server: key, source
     return (
       <mui.Dialog
-          title="Create Job"
-          actions={actions}
-          modal={true}
-          open={this.state.open}
-      >
+        title="Create Job"
+        modal={true}
+        open={this.state.open}
+        actions={[
+          <mui.FlatButton label="Cancel" secondary={true} onTouchTap={this.handleClose}/>,
+          <mui.FlatButton label="Submit" type="submit" primary={true} disabled={!this.state.canSubmit} onTouchTap={() => this.refs.form.submit()}/>
+        ]} >
         <mui.ClearFix>
-          <mui.TextField ref='title' required={true} type='text' hintText="*Title" autofocus={true} fullWidth={true}/>
-          <mui.TextField ref='company' required={true} type='text' hintText="*Company" fullWidth={true}/>
-          <mui.TextField ref='location' type='text' hintText="Location" />
-          <mui.Checkbox ref="remote" label="Remote"/><br/>
-          {/*remote*/}
-          {/*url?*/}
-          <mui.TextField ref='tags' type='text' hintText="*Skills/Tags (comma-delimited)" multiLine={true} fullWidth={true}/>
-          <mui.TextField ref='money' type='number' hintText="Money (budget, salary, hourly-rate, etc)" fullWidth={true}/>
-          <mui.TextField ref='description' required={true} hintText="*Description" multiLine={true} fullWidth={true}/>
+          <Formsy.Form
+            ref="form"
+            onValid={() => this.setState({canSubmit: true})}
+            onInvalid={() => this.setState({canSubmit: false})}
+            onValidSubmit={this.submitForm}>
+            <fui.FormsyText name='title' required hintText="*Title" fullWidth={true}/>
+            <fui.FormsyText name='company' required hintText="*Company" fullWidth={true}/>
+            <fui.FormsyText name='location' hintText="Location" fullWidth={true}/>
+            <fui.FormsyCheckbox name='remote' label="Remote"/>
+            <fui.FormsyText name='tags' required hintText="*Skills/Tags (comma-delimited)" fullWidth={true}/>
+            <fui.FormsyText name='money' validations='isAlpha' validationError="Please enter numbers" hintText="Money (budget, salary, hourly-rate, etc)" fullWidth={true}/>
+            <fui.FormsyText name='description' required hintText="*Description" multiline={true} rows={2} fullWidth={true}/>
+          </Formsy.Form>
         </mui.ClearFix>
       </mui.Dialog>
     )
@@ -49,13 +47,8 @@ export default class CreateJob extends React.Component {
   handleOpen = () => this.setState({open: true});
   handleClose = () => this.setState({open: false});
 
-  _createJob = () => {
+  submitForm = (body) => {
     this.handleClose();
-    let body = _.reduce('title company location remote tags money description'.split(' '), (m, v) => {
-      let el = this.refs[v];
-      m[v] = el.isChecked ? el.isChecked() : el.getValue();
-      return m;
-    },{});
     _fetch('jobs', {method:"POST", body}).then(this.props.onCreate);
   };
 }
