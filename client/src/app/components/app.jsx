e6d07a15bd51ca5075fb1a94e3a0664f35e59662
Tyler Renelle <tylerrenelle@gmail.com>
2016-03-05 14:04:21 -0500

    use toolbar instead of appbar + leftnav

--- client/src/app/components/app.jsx
+++ client/src/app/components/app.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import mui from 'material-ui';
+import {
+  SocialPerson,
+  CommunicationEmail,
+  PlacesBusinessCenter
+} from 'material-ui/lib/svg-icons';
+//import FontIcon from 'material-ui/lib/font-icon';
+import mui, {
+  Toolbar,
+  ToolbarGroup,
+  DropDownMenu,
+  MenuItem,
+  ToolbarTitle,
+  FontIcon,
+  IconMenu,
+  ToolbarSeparator,
+  RaisedButton,
+  IconButton,
+  FlatButton,
+} from 'material-ui';
 import _ from 'lodash';
 import Footer from './footer';
-
-const menuItems = [
-  {route: 'jobs/inbox', text: 'Inbox' },
-  {route: 'jobs/applied', text: 'Applied' },
-  {route: 'jobs/liked', text: 'Liked' },
-  //{route: 'jobs/disliked', text: 'Disliked' },
-  //{ type: mui.MenuItem.Types.SUBHEADER },
-  {route: 'employer', text: 'Employer' },
-  {route: 'messages', text: 'Messages' },
-  {route: 'profile', text: 'Profile' },
-  {route: 'logout', text:"Logout"}
-];
 
 export default class App extends React.Component {
   constructor(props) {
@@ -22,24 +28,41 @@ export default class App extends React.Component {
   }
 
   render(){
-    //let {route} = this.props;
-    let route = this.props.location.pathname.replace(/^\//,'');
+    let title = { //FIXME this is dirty. Use state instead?
+      'jobs/inbox': 'Inbox',
+      'jobs/applied': 'Applied',
+      'jobs/liked': 'Liked',
+      'employer': 'Employer',
+      'messages': 'Messages',
+      'profile': 'Profile',
+      'logout': "Logout"
+    }[this.props.location.pathname.replace(/^\//,'')] || 'Jobpig';
     return (
       <div>
-        <mui.AppBar
-          title={_.find(menuItems, {route}).text}
-          onLeftIconButtonTouchTap={this.handleToggle}
-          onRequestChange={open => this.setState({open})} />
-        <mui.LeftNav
-          open={this.state.open}
-          docked={false} >
-          {menuItems.map((v,i) =>
-            <mui.MenuItem
-                primaryText={v.text}
-                onTouchTap={() => this._goto(v.route)}
-                key={'menu-item-' + i} />
-          )}
-        </mui.LeftNav>
+        <Toolbar>
+          <ToolbarTitle text={title} />
+          <ToolbarGroup float="right">
+            <DropDownMenu value='inbox' onChange={(evt, idx, val) => this._goto('jobs/' + val)}>
+              <MenuItem value='inbox' primaryText="Inbox"/>
+              <MenuItem value='applied' primaryText="Applied" />
+              <MenuItem value='liked' primaryText="Liked" />
+            </DropDownMenu>
+            <IconButton touch={true} tooltip='Employer' onTouchTap={() => this._goto('employer')}>
+              <PlacesBusinessCenter />
+            </IconButton>
+            <IconButton touch={true} tooltip='Messages' onTouchTap={() => this._goto('messages')}>
+              <CommunicationEmail />
+            </IconButton>
+            <IconMenu
+              tooltip="Profile"
+              iconButtonElement={<IconButton touch={true}><SocialPerson /></IconButton>} >
+              <MenuItem primaryText="Profile" onTouchTap={() => this._goto('profile')} />
+              <MenuItem primaryText="Logout" onTouchTap={() => this._goto('logout')} />
+            </IconMenu>
+
+          </ToolbarGroup>
+        </Toolbar>
+
         {this.props.children}
         <Footer />
       </div>
