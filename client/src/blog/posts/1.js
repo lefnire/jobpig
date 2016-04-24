223c763b30e7063533541741e81e9fe48a813a73
Tyler Renelle <tylerrenelle@gmail.com>
2016-04-24 10:15:33 -0400

    Blog cleanup

--- client/src/blog/posts/1.js
+++ client/src/blog/posts/1.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 let title = 'Best Tech Job Boards (+Remote)';
 let date = '2016-04-20';
 
-let teaser = <p>There are hundreds, if not thousands, of online job boards - and many (most?) for tech. They vary <em>wildly</em> in quality (by which I mean serious, well-paying employers). In this post I'll cover the best boards from my experience. I'll edit this post periodically, and eventually take comments so people can recommend their favorites / correct me.</p>;
+let teaser = <p>There are hundreds, if not thousands, of online job boards - and many (most?) for tech. They vary in quality (by which I mean serious, well-paying employers). In this post I'll cover the best boards from my experience. I'll edit this post periodically, and eventually take comments so people can recommend their favorites / correct me.</p>;
 
 let body = <div>
   {teaser}
@@ -18,7 +18,7 @@ let body = <div>
     <li>{l('Dice', 'http://www.dice.com/')}</li>
     <li>{l('Jobpig', 'https://jobpigapp.com/')}</li>
   </ul>
-  <p>These are my personal favorites. I've found fantastic-quality gigs from these boards. Their search functions are fine-grained and tech-geared. Employers who post here are hip and with the latest technology trends. Well-funded startups and the like.</p>
+  <p>My personal favorites. I've found fantastic-quality gigs from these boards. Their search functions are fine-grained and tech-geared. Employers who post here are hip and with the latest technology trends. Well-funded startups and the like.</p>
   <h4>The "OK"</h4>
   <ul>
     <li>{l('Indeed','https://indeed.com')}</li>
@@ -27,39 +27,51 @@ let body = <div>
     <li>{l('Monster','http://www.monster.com/')}</li>
     <li>{l('CareerBuilder','https://careerbuilder.com')}</li>
   </ul>
-  <p>Everyone knows Monster & Indeed - those are the heavy-hitters. But these are general boards, not tech-geared. Their search functions are crude compared to the previous list which let you filter - or at least shows you - skills, salary, equity, etc. Their tech postings tend to be by very large "legacy" companies (Java, .NET). YMMV, definitely give them a shot - you may have a different experience.</p>
+  <p>Everyone knows Monster & Indeed - those are the heavy-hitters. But these are general boards, not tech-geared. Their search functions are cruder than the previous list which let you filter - or at least shows you - skills, salary, equity, etc. Their tech postings tend to be by large "legacy" companies. YMMV, definitely give them a shot - you may have a different experience.</p>
   <h3>Remote</h3>
-  <p>If you're looking for remote work - contract / freelance (digital nomad); or even full-time W2, but ain't up-n-moving to San Francisco, here are tips.</p>
+  <p>If you're looking for remote work - contract / freelance / digital nomad; or full-time remote W2 - here are tips.</p>
   <h4>The Good</h4>
-  <p>Firstly, check out {l('NoDesk', 'http://nodesk.co/')} and {l('Awesome Remote Job', 'https://github.com/lukasz-madon/awesome-remote-job/')} - curated resources for remote workers, and lists of remote job boards. Afterwards decide:</p>
-  <ol>
-    <li>Are you looking for a serious, high-quality remote W2? Use "The Good" from "General" above - they double as quality remote-work sites. The jobs indicate whether they're remote-friendly.</li>
-    <li>Or are you looking for more contractor / freelancer gigs (think beach-side digital nomads). If so, these are my favorite boards:</li>
-  </ol>
+  <p>Firstly, see {l('NoDesk', 'http://nodesk.co/')} and {l('Awesome Remote Job', 'https://github.com/lukasz-madon/awesome-remote-job/')} - curated resources for remote workers, and lists of remote job boards. Afterwards decide:</p>
+
+    {/*
+      No Fluff Jobs filter -> "remote"
+      Skip the Drive === simplyhired
+      Workana Freelance Job Board in Spanish and Portuguese
+      offsite.careers - dead
+      remoteworkhunt - dead
+    */}
+
   <ul>
-    <li>{l('Working Nomads', 'http://www.workingnomads.co')}</li>
-    <li>{l('WFH.io', 'https://www.wfh.io')}</li>
-    <li>{l('We Work Remotely', 'https://weworkremotely.com')}</li>
-    <li>{l('Virtual Vocations', 'https://www.virtualvocations.com')}</li>
-    <li>{l('Landing.jobs', 'https://landing.jobs')}</li>
-    <li>{l('Jobspresso', 'https://jobspresso.co')}</li>
-    <li>{l('Jobmote', 'http://jobmote.com')}</li>
+    <li>See <em>General > The Good</em> from above - these double as quality remote-work sites. The jobs indicate whether they're remote-friendly.</li>
     <li>{l('Authentic Jobs', 'https://authenticjobs.com')}</li>
-    <li>{l('RemoteOK', 'https://remoteok.io')}</li>
     <li>{l('Behance','https://www.behance.net/joblist')} (for designers)</li>
+    <li>{l('Dribbble','https://dribbble.com/jobs')} (for designers)</li>
+    <li>{l('Front-end Developer Jobs', 'http://frontenddeveloperjob.com/')}</li>
+    <li>{l('Jobmote', 'http://jobmote.com')}</li>
+    <li>{l('Jobspresso', 'https://jobspresso.co')}</li>
+    <li>{l('Landing.jobs', 'https://landing.jobs')}</li>
+    <li>{l('RemoteCoder', 'https://remotecoder.io/')} (aggregator)</li>
+    <li>{l('Remotely Awesome Jobs', 'https://www.remotelyawesomejobs.com/')} (aggregator)</li>
+    <li>{l('RemoteOK', 'https://remoteok.io/')} (aggregator)</li>
+    <li>{l('Remotive Jobs', 'http://jobs.remotive.io/')}</li>
+    <li>{l('Virtual Vocations', 'https://www.virtualvocations.com')}</li>
+    <li>{l('We Work Remotely', 'https://weworkremotely.com')}</li>
+    <li>{l('WFH.io', 'https://www.wfh.io')}</li>
+    <li>{l('Who is hiring', 'https://whoishiring.io/')} (aggregator)</li>
+    <li>{l('Working Nomads', 'http://www.workingnomads.co')} (aggregator)</li>
   </ul>
+
+  <p>If you target a specific technology, check first if there's a job board for to that tech - they tend to be great insiders' listings. Eg {l('Go', 'http://www.golangprojects.com/golang-remote-jobs.html')}, {l('Python','http://www.pythonjobs.com/')}, {l('Ionic','http://jobs.ionic.io/')}, etc.</p>
   <h4>The OK</h4>
-  <p>The following boards "mean well," but but have never ended up landing me solid work:</p>
+  <p>The following boards "mean well," but through which I've never found me solid work:</p>
   <ul>
-    <li>{l('Gun.io', 'https://gun.io/')} - great system, but their job churn is too slow. I rarely see new posts.</li>
-    {/*<li>{l('remoteworkhunt', 'http://www.remoteworkhunt.com')}</li>*/}
-    {/*<li>{l('offsite.careers', 'http://offsite.careers')}</li>*/}
-    <li>{l('PeoplePerHour', 'http://www.peopleperhour.com/')} - started off fantastic, but began leaning towards offshore, rendering rates non-competitive.</li>
-    <li>{l('Hasjob', 'https://hasjob.co')}</li>
+    <li>{l('Gun.io', 'https://gun.io/')} - great system, but their job churn is slow. I rarely see new posts.</li>
+    <li>{l('PeoplePerHour', 'http://www.peopleperhour.com/')} - started off fantastic, but began leaning offshore, rendering rates non-competitive.</li>
+    <li>{l('Hasjob', 'https://hasjob.co')} - my experience was mostly low-paying offshore jobs.</li>
     <li>{l('Flexjobs', 'https://www.flexjobs.com')}</li>
   </ul>
   <h4>The Ugly</h4>
-  <p>And finally, the penny jobs. Posted by upstart college-grads looking for a $5 Facebook clone; your competition is offshore, so balk all you want, they'll get what they requested. While I disparage these boards for finding remote work yourself, they're great for hiring talent for <em>your</em> projects! I use these sites for commissioning logos, icons, and discrete chunks of small independent work.</p>
+  <p>And finally, the penny boards. Posted by upstart college-grads looking for a $5 Facebook clone; your competition is offshore, so balk all you want, they'll get what they requested. While I disparage these boards for finding quality gigs yourself, they're great for two things. (1) Building your portfolio in unfamiliar technology (you're bootstrapping or pivoting). (2) Hiring talent for <em>your</em> projects - I use these sites for commissioning logos, icons, and discrete chunks of small independent work.</p>
   <ul>
     <li>oDesk / Elance / Upwork (all the same, rebranded as Upwork)</li>
     <li>Freelancer</li>
--- client/src/blog/posts/3.js
+++ client/src/blog/posts/3.js
@@ -1,7 +1,7 @@
 import {l} from './helpers';
 import {Link} from 'react-router';
 
-let title = 'Do I Need to Code to Work Remotely?';
+let title = 'Do I Need to Program to Work Remotely?';
 let date = '2016-04-20';
 
 let teaser = <p>A common question from aspiring digital nomads: "do I have to be a programmer?" The answer is of course no, not technically; but you should strongly consider it.</p>;
@@ -9,7 +9,7 @@ let teaser = <p>A common question from aspiring digital nomads: "do I have to be
 let body = <div>
   {teaser}
 
-  <p>First, I don't just mean C++; I mean web & mobile development and design, graphic design, video / content / email marketing, project management - basically high tech... for a tech company... based in San Francisco. This compared to non-tech jobs like WWOOFing, ESL; and tech, but cut-rate competitive bits like photography, blogging, and book-writing. There'll be disagreement by well-paid professionals in fields I disparage. I'd bet they've been doing it long enough to establish themselves as highly-sought experts. Tech has a much lower barrier to entry for higher, faster yield. This post is for readers interested in just that.</p>
+  <p>First, I don't just mean C++; I mean web & mobile development and design, graphic design, marketing - basically high tech... for a tech company... based in San Francisco. This compared to non-tech jobs like WWOOFing, ESL; and tech, but cut-rate competitive bits like photography, blogging, and book-writing. There'll be disagreement by well-paid professionals in fields I disparage. I'd bet they've been doing it long enough to establish themselves as highly-sought experts. Tech has a much lower barrier to entry for higher, faster yield. This post is for readers interested in just that.</p>
 
   <p>As you're reading this, consider {l("one blogger's yearly travel cost", 'http://www.neverendingfootsteps.com/2016/02/16/how-much-does-it-cost-to-travel-the-world-for-a-year-my-2015-expenses/')} of $20k. Bear in mind she did it <em>on the cheap</em> (my own best-efforts were closer to $40k), so pad a bit.</p>
 
@@ -27,12 +27,12 @@ let body = <div>
   <ul>
     <li><b>Freelance Writer, Blogger.</b> A blogger's salary {l('is $40k','http://www.payscale.com/research/US/Job=Blogger/Salary')}, assuming you're blogging / writing for another company. Which actually makes this viable remote work, if you're so doing! The concern I want to raise is that many bloggers plan a travel blog to strike gold. Yes that happens, but you can't count on it. That's the big break.</li>
     <li><b>Photographer.</b> {l('$24/h','http://www.payscale.com/research/US/Job=Freelance_Photographer/Hourly_Rate')}. Per above, are you doing photography for a home-based company; or are you planning on travel photography?</li>
-    <li><b>eBook Publishing.</b> This one makes me flinch. Ask any writer how much time they put into their latest work, and how much money they made.</li>
+    <li><b>eBook Publishing.</b> Similar to the above, this can make a lot of money... in the jackpot. If you're going to count on this revenue flow, be sure it's flowing first.</li>
     <li><b>Virtual Assistant / Customer Service.</b> {l('$16/h', 'http://www.payscale.com/research/US/Job=Virtual_Assistant/Hourly_Rate')}, and boy does it sound fun!</li>
   </ul>
 
   <h4>Tech</h4>
-  <p>Here's the thing about code - it's a lot easier than you think; you're psyching yourself out! I've read many accounts of waiter-gone-iOS-apps who put in 1-3 months' learning before cold-turkey. I've personally converted and coached code newbies, seeing 3 months as a standard turnaround. One month in cases where people dedicated 40h/wk learning. You don't need school ({l('I strongly discourage school!','https://www.reddit.com/r/digitalnomad/comments/2zhlmt/what_kind_of_webdev_is_better_for_a_nomad/cpvqjxd')}), just dedication. The starting rate is $40/h 1099. {l('iOS Developer, $80k median','http://www.payscale.com/research/US/Job=iOS_Developer/Salary')}. You're not limited by any factor save wifi (more than can be said for most remote jobs). You can hop countries as often as you want - you're working for home-based companies. It's the most liberating digital nomad experience (from experience!) there is, I really mean it.</p>
+  <p>Here's the thing about code - it's a lot easier than you think; you're psyching yourself out! I've read many accounts of waiter-gone-iOS-apps who put in 1-3 months' learning before cold-turkey. I've personally converted and coached code newbies, seeing 3 months as a standard turnaround; one month in cases where people dedicated 40h/wk learning. You don't need school ({l("don't do school!",'https://www.reddit.com/r/digitalnomad/comments/2zhlmt/what_kind_of_webdev_is_better_for_a_nomad/cpvqjxd')}), just dedication. A common starting web dev rate I see is $40/h 1099. {l('iOS Developer, $80k median','http://www.payscale.com/research/US/Job=iOS_Developer/Salary')}. That's double the best positions above! You're not limited by any factor save wifi (more than can be said for most remote jobs). You can hop countries as often as you want - you're working for home-based companies. It's the most liberating digital nomad experience, from experience!</p>
   <p>I think when people consider coding with fear, they're thinking of backend engineers. Target front-end development / design, it has a very low barrier to entry. <Link to="/2">Start here!</Link></p>
 </div>;
 
